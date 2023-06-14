ff95b06fd6889ef545bad8c01509e0a949269cfe
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-06-14 15:06:40 +0400

    trying to host our website using Nginx in Docker container

--- src/main/java/com/alialsubhi/learnspringboot/CourseController.java
+++ src/main/java/com/alialsubhi/learnspringboot/CourseController.java
@@ -3,19 +3,23 @@ package com.alialsubhi.learnspringboot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class CourseController {
 	
 	//http://localhost:8080/courses
-	@RequestMapping("/courses")
+	@GetMapping("/courses")
 	public List<Course> retriveAllCourses(){
 		return Arrays.asList(
 				new Course(1,"Learn AWS","SelfLearing"),
-				new Course(2,"Learn DevOps","SelfLearing")
-				);
+				new Course(2,"Learn DevOps","SelfLearing"),
+				new Course(3,"Ali AlSubhi","TESTING"),
+				new Course(4,"Said Zutti","TESTING")
+		);
 		
 	}
 
